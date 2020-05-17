c5cbaa5f95f2581cdbc601289ab5c2799dd6639a
Petras <seventhnexus@gmail.com>
2020-05-17 22:26:18 +0300

    Add course material

---
+++ Section Exam - Leap Year/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section Exam - Leap Year")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section Exam - Leap Year")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b497c15-58d7-48a6-a033-3ff552bbad1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Section Exam - Leap Year/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section_Exam___Leap_Year
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Trace.Write("Please enter a year value: ");
+            int enteredYear = Convert.ToInt16(Console.ReadLine());
+            //int enteredYear = 400;
+            int divBy4 = enteredYear % 4;
+            int divBy100 = enteredYear % 100;
+            int divBy400 = enteredYear % 400;
+
+
+            if (divBy4 == 0 && divBy100 == 0 && divBy100 == 0)
+            {
+                Console.WriteLine("Leap year: yes");
+            }
+            if (divBy4 == 0 && divBy100 == 0)
+            {
+                Console.WriteLine("Leap year: yes");
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
